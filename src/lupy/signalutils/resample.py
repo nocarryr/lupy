0a5c05f462d93cb081c020ecb4f38a65ccc05e3e
nocarryr <matt@nomadic-recording.com>
2026-01-02 15:32:14 -0600

    Add comment about what appears to be an invalid cast

--- src/lupy/signalutils/resample.py
+++ src/lupy/signalutils/resample.py
@@ -43,6 +43,9 @@ def calc_tp_fir_win(upsample_factor: int) -> Float1dArray:
     max_rate = max(up, down)
     f_c = 1 / max_rate
     half_len = 10 * max_rate
+
+    # Casting to str because firwin's type hints are not compatible with the
+    # implementation.
     window = cast(str, ('kaiser', 5.0))
     h = firwin(
         half_len + 1,       # len == 41 with upsample factor of 4
