ad92ab974572b6306c76fac962ee3d3a3775bf2b
nocarryr <matt@nomadic-recording.com>
2026-02-21 14:08:44 -0600

    Add tests for Slice object

--- tests/test_sampling.py
+++ tests/test_sampling.py
@@ -3,9 +3,128 @@ from __future__ import annotations
 import pytest
 import numpy as np
 
-from lupy.sampling import Sampler, TruePeakSampler, calc_buffer_length
+from lupy.sampling import Sampler, TruePeakSampler, calc_buffer_length, Slice
 
-# def test_slice():
+
+
+def test_slice_non_overlap():
+    src_array_shape = (2, 48000)
+    chunk_size = 1000
+    overlap = 0
+    num_chunks = src_array_shape[1] // chunk_size
+    assert src_array_shape[1] % chunk_size == 0
+
+    src_array = np.zeros(src_array_shape, dtype=int)
+    src_array[0, :] = np.arange(src_array_shape[1])
+    src_array[1, :] = np.arange(src_array_shape[1]) * 10
+    expected_array = np.reshape(src_array, (2, num_chunks, chunk_size))
+
+    slc = Slice(
+        step=chunk_size,
+        max_index=num_chunks-1,
+        overlap=overlap,
+    )
+
+    assert slc.calc_shape(src_array, axis=1) == (2, chunk_size)
+
+    for i in range(num_chunks):
+        assert slc.index == i
+        assert not slc.is_wrapped(src_array, axis=1)
+        chunk = slc.slice(src_array, axis=1)
+        assert chunk.shape == (2, chunk_size)
+        assert np.array_equal(chunk, expected_array[:, i, :])
+        slc.increment(src_array, axis=1)
+
+    assert slc.index == 0
+    chunk = slc.slice(src_array, axis=1)
+    assert chunk.shape == (2, chunk_size)
+    assert np.array_equal(chunk, expected_array[:, 0, :])
+
+
+def test_slice_with_overlap():
+    N = 48000
+    src_array_shape = (2, N)
+    chunk_size = 1000
+    overlap_pct = 0.75
+    step = 1 - overlap_pct
+    step_count = int(step * chunk_size)
+
+    def get_chunk_indices(j: int) -> tuple[int, int]:
+        start_ix = int(chunk_size * (j * step))
+        end_ix = int(chunk_size * (j * step + 1))
+        return start_ix, end_ix
+
+    # j \in 0, 1, \ldots, \frac{N - chunk\_size}{chunk\_size \cdot step}
+    num_chunks = int(np.floor((N - chunk_size) / (chunk_size * step))) + 1
+
+    src_array = np.zeros(src_array_shape, dtype=int)
+    src_array[0, :] = np.arange(src_array_shape[1])
+    src_array[1, :] = np.arange(src_array_shape[1]) * 10
+
+    expected_array = np.zeros((2, num_chunks, chunk_size), dtype=int)
+    for i in range(num_chunks):
+        start_ix, end_ix = get_chunk_indices(i)
+        values = np.arange(start_ix, end_ix)
+
+        expected_array[0, i, :] = values
+        expected_array[1, i, :] = values * 10
+
+    slc = Slice(
+        step=chunk_size,
+        max_index=0,
+        overlap=step_count,
+    )
+
+    assert slc.calc_shape(src_array, axis=1) == (src_array_shape[0], chunk_size)
+
+    for i in range(num_chunks):
+        start_ix, end_ix = get_chunk_indices(i)
+        assert slc.index == i
+        if slc.end_index > src_array.shape[1]:
+            assert slc.is_wrapped(src_array, axis=1)
+        else:
+            assert not slc.is_wrapped(src_array, axis=1)
+            assert slc.start_index == start_ix
+            assert slc.end_index == end_ix
+        chunk = slc.slice(src_array, axis=1)
+        assert chunk.shape == (src_array_shape[0], chunk_size)
+        assert np.array_equal(chunk, expected_array[:, i, :])
+        slc.increment(src_array, axis=1)
+
+    assert end_ix == src_array.shape[1]
+    i = num_chunks
+    real_i = num_chunks
+    wrap_completed = False
+    while i < num_chunks * 2:
+        start_ix, end_ix = get_chunk_indices(i)
+        is_wrapped = start_ix < src_array.shape[1] and end_ix > src_array.shape[1]
+        assert slc.is_wrapped(src_array, axis=1) == is_wrapped
+        if is_wrapped:
+            if wrap_completed:
+                break
+            assert slc.index == i
+            chunk_start = src_array[:, start_ix:]
+            chunk_end = src_array[:, :end_ix % src_array.shape[1]]
+            chunk_expected = np.concatenate((chunk_start, chunk_end), axis=1)
+        else:
+            if not wrap_completed:
+                real_i = 0
+                wrap_completed = True
+            start_ix, end_ix = get_chunk_indices(real_i)
+            assert slc.index == real_i
+            chunk_expected = expected_array[:, real_i, :]
+
+        assert slc.start_index == start_ix
+        assert slc.end_index == end_ix
+        chunk = slc.slice(src_array, axis=1)
+        assert chunk.shape == (src_array_shape[0], chunk_size)
+        assert np.array_equal(chunk, chunk_expected)
+
+        slc.increment(src_array, axis=1)
+        i += 1
+        real_i += 1
+
+    assert wrap_completed
 
 
 
