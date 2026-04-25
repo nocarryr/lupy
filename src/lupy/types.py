61d6cf6ee63735a8d0e1de0e806ef55a7b62d0a5
nocarryr <matt@nomadic-recording.com>
2026-04-25 15:12:16 -0500

    Remove unused type alias

--- src/lupy/types.py
+++ src/lupy/types.py
@@ -31,7 +31,6 @@ Complex = np.complex128
 
 
 
-_AnyDtype: TypeAlias = np.dtype[Any]
 DType_t = TypeVar("DType_t", bound=np.dtype[Any])
 DType_co = TypeVar("DType_co", bound=np.dtype[Any], covariant=True)
 """"""
