977639bfd0c6e6c139186e503419dbb2ab1f7e8a
nocarryr <matt@nomadic-recording.com>
2026-05-03 18:40:02 -0500

    Correct typing for `_sosfilt` input array

--- src/lupy/signalutils/_sosfilt.pyi
+++ src/lupy/signalutils/_sosfilt.pyi
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import numpy as np
 
-from ..types import Float2dArray, SosZI, SosCoeff
+from ..types import SosZI, SosCoeff
 
-def _sosfilt(sos: SosCoeff, x: np.ndarray[tuple[int, int], np.dtype[np.floating|np.complexfloating]], zi: SosZI) -> None:
+def _sosfilt(sos: SosCoeff, x: np.ndarray[tuple[int, int], np.dtype[np.floating]], zi: SosZI) -> None:
     ...
